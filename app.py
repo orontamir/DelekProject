86245273177f8988c8c9f9004d40d97040c443a3
orontamir <orontamir@gmail.com>
2020-10-25 14:49:52 +0200

    add delek images

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
